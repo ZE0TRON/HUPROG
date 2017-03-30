8bd3325d50abc48b1a0b94acb3917ccb4a2ef6fd
Okan ALAN <okanalan97.14@gmail.com>
2017-03-30 22:07:16 +0300

    Update okan.py
    n,m degiskenler duzelttim

--- ON-ELEME/SORULAR/Dev Ve Okan/okan.py
+++ ON-ELEME/SORULAR/Dev Ve Okan/okan.py
@@ -5,7 +5,7 @@ def factorial(n):
         return(factorial(n-1)*n)
 q=int(input())
 for test in range(q):
-    N,M=map(int,input().split())
+    n,m=map(int,input().split())
     for i in range(M):
         input()
 print(factorial(n+m-2)//(factorial(n-1)*factorial(m-1))%((10**9)+7))
