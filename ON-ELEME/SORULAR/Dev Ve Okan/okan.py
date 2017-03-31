62cc8c297fb8ff5f6faee7f2b7f645c1ca8d9f11
Okan ALAN <okanalan97.14@gmail.com>
2017-04-01 00:13:03 +0300

    Update okan.py
    dynamic faktoriyel okan ve dev

--- ON-ELEME/SORULAR/Dev Ve Okan/okan.py
+++ ON-ELEME/SORULAR/Dev Ve Okan/okan.py
@@ -1,9 +1,12 @@
 def factorial(n):
-    if(n==1):
-        return 1
-    else:
-        return(factorial(n-1)*n)
+    for i in range(1,n+1):
+        if i not in fakto.keys():
+            fakto[i]=fakto[i-1]*i
+    return fakto[n]
 q=int(input())
+fakto={}
+fakto[0]=1
+fakto[1]=1
 for test in range(q):
     n,m=map(int,input().split())
     for i in range(n+1):
