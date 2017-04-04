c6f84a560c968aecdba2d5bbe38d9356ad11181a
ze0tron <cmpbilge@gmail.com>
2017-04-04 23:18:04 +0300

    Cilginstring fixed but slow

--- Final/cilginstring/cilginstring.py
+++ Final/cilginstring/cilginstring.py
@@ -1,7 +1,24 @@
 from collections import defaultdict
-def negativer():
+"""def negativer():
     return -1
 DP=defaultdict(negativer)
+"""
+def BFS(start, end):
+    q=[(start,0)]
+    travelled=[]
+    while len(q)!=0:
+        current,counter = q.pop(0)
+        if current==end:
+            return counter
+        for i in range(0,N):
+            for j in range(i,N):
+                elem=current[:i]+current[j:]+current[i:j]
+                if elem not in travelled:
+                    if(counter<len(current)):
+                        q.append((elem,counter+1))
+                        travelled.append(elem)
+    return float('inf')
+"""
 def DParak(a,counter):
     if(a==aranan):
         return counter
@@ -20,8 +37,10 @@ def DParak(a,counter):
         return z
     else:
         return DP[a]
+"""
 Q=int(input())
 for test in range(Q):
     N=int(input())
     s1 ,aranan = input().split()
-    print(DParak(s1,0))
+    print(s1,aranan)
+    print(BFS(s1,aranan))
