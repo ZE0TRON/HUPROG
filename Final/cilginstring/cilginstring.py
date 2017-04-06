1dc576b97947805fcbd2112d913cf4dbffd616f3
ze0tron <cmpbilge@gmail.com>
2017-04-06 13:46:40 +0300

    MeowMeMeowMeowMeowMewMewMeMeMeowMeMewMewMewMew

--- Final/cilginstring/cilginstring.py
+++ Final/cilginstring/cilginstring.py
@@ -4,28 +4,28 @@ def negativer():
     return -1
 DP=defaultdict(negativer)
 
-def BFS(start, end):
+def BFS(start, end,blok):
     q=[(start,0,end)]
     travelled=[]
     while len(q)!=0:
         current,counter,end = q.pop(0)
-        counter2=0
-        for i in range(len(current)):
-            if current[i]==end[i] :
-                counter2+=1
-            else:
-                break
-        if counter2==len(current):
+        #counter2=0
+        #for i in range(len(current)):
+        #    if current[i]==end[i] :
+        #        counter2+=1
+        #    else:
+        #        break
+        if current==end:
             return counter
-        current=current[counter2:]
-        end=end[counter2:]
-        for i in range(0,N):
-            for j in range(i,N):
-                elem=current[:i]+current[j:]+current[i:j]
-                if elem not in travelled:
-                    if(counter<len(current)):
-                        q.append((elem,counter+1,end))
-                        travelled.append(elem)
+        #current=current[counter2:]
+        #end=end[counter2:]
+        for i in range(0,N-2):
+            j=i+blok
+            elem=current[:i]+current[j:]+current[i:j]
+            if elem not in travelled:
+                if(counter<len(current)):
+                    q.append((elem,counter+1,end))
+                    travelled.append(elem)
     return float('inf')
 """
 def DParak(a,counter):
@@ -56,7 +56,8 @@ def DParak(a,counter):
 """
 Q=int(input())
 for test in range(Q):
+    blok=int(input())
     N=int(input())
     s1 ,aranan = input().split()
     print(s1,aranan)
-    print(BFS(s1,aranan))
+    print(BFS(s1,aranan,blok))
