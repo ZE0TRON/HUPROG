c58e9be7ee3b7cca29735e89a833345e2310ac97
BatuhanHU <batuhan@hacettepe.edu.tr>
2017-04-06 13:37:05 +0300

    fixed cilgin string generator for pilge

--- Final/cilginstring/cilginStringinp.py
+++ Final/cilginstring/cilginStringinp.py
@@ -12,18 +12,22 @@ print("string uzunluğu min max: ")
 minS,maxS = map(int,input().rstrip().split(" "))
 print("min, max karıştırma miktarı")
 minK,maxK = map(int,input().rstrip().split(" "))
+print("min, max alınacak parça uzunluğu: (minP maxP)")
+minP,maxP = map(int,input().rstrip().split(" "))
 
 print(q,file=out)
 for _ in range(q):
     uzunluk = randint(minS,maxS)
     kelime = ""
+    nekadar = randint(minP,maxP)
+    print(nekadar,file=out)
     for _ in range(uzunluk):
         kelime += (choice(ascii_lowercase))
     print(uzunluk,file=out)
     karistirma = randint(minK,maxK)
     temp=kelime
     for _ in range(karistirma):
-        start = randint(0,uzunluk - 2)
-        end = randint(start,uzunluk - 1)
+        start = randint(0,uzunluk - nekadar - 1)
+        end = start + nekadar
         kelime = kelime[:start] + kelime[end:] + kelime[start:end]
     print(temp,kelime,file=out)
