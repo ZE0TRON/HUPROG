a18b7e4a6e3752987e1280f4d8bb5fdfc1ea6629
ze0tron <cmpbilge@gmail.com>
2017-04-06 11:00:13 +0300

    Sanchuez

--- Final/cilginece/cilginEceCozum3.py
+++ Final/cilginece/cilginEceCozum3.py
@@ -1,7 +1,9 @@
+def xor(a,b):
+    return a^b
 q=int(input())
 for _ in range(q):
     N = int(input())
-    liste = list(map(int,input().rstrip().split()))
+    liste = map(xor,list(map(int,input().rstrip().split())))
     anlamsızSayı = liste[0]
     for elem in liste[1:]:
         anlamsızSayı = anlamsızSayı ^ elem
